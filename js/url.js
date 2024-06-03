ae4c7613d016e88551bf07048fb130952ea68d22
christyuda <chrisyuda@ulbi.ac.id>
2024-06-04 01:40:50 +0700

    feaet : execution time

--- js/url.js
+++ js/url.js
@@ -1,2 +1,3 @@
 export const endpointDaftar = "http://localhost:3000/api/auth/register"
 export const endpointLogin = "http://localhost:3000/api/auth/login"
+export const endpointJavascriptTypes = "http://localhost:3000/api/all-js"
